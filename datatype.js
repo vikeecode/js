89e80e505d6c789446e8e9d6faa61a21b8772d0d
vikas <vikeepandit178@gmail.com>
2024-04-01 17:34:00 +0530

    this is my secound

---
+++ datatype.js
@@ -0,0 +1,24 @@
+ "use strict"//treate all js code in newely version
+ //alert(3+2)// we are using nodejs ,not browser
+ console.log(3+
+    3)//code readability that should be high not write these kind of code
+    console.log("vikas");
+    let name="vikas"//this is string
+    let age=21;//this is number
+    let isLoggedIn=true//this is bollean
+
+    //number=>(number range is js are 2 to power 53)
+    //and when you use out of range than use bigInt
+    //string it is use in ("")
+    //boolean=> here is only two values true/false
+    //null is actually null is a stand alone value in js
+    //undefined=>
+    //symbol to find a uniqueness
+
+
+    
+
+    //object
+    console.log(typeof age);
+    console.log(typeof null);
+    console.log(typeof undefined);
