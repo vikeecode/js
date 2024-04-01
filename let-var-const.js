a520900db46820029426452fa3f1fd2f763f5919
vikas <vikeepandit178@gmail.com>
2024-04-01 17:05:14 +0530

    this is my first code

---
+++ let-var-const.js
@@ -0,0 +1,16 @@
+const accountId =23456;
+let emailId = "vikeepandit178@gmail.com";
+var passwordId ="823750";
+cityName = "gurgam";
+passwordId=124498;
+emailId="hcbc@gmail.com";
+cityName="pune";
+let accountState;
+
+
+// accountId=2;//not allowed
+console.table([accountId,emailId,passwordId,cityName,accountState])
+/*
+perfernot use var
+because of the issue in block and functional scope
+*/
