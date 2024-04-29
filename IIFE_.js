ff28e1dc42616698811f58e5f0cc07a5234cb6b1
vikas <vikeepandit178@gmail.com>
2024-04-29 21:37:49 +0530

    this is iife in js

---
+++ IIFE_.js
@@ -0,0 +1,16 @@
+//Immediately Invoked Function Expression (IIFE)
+
+//if your code not polluted in global scoope than use a iife
+//jo function turant execute ho jye and global scope ka pollution aur global scope variable or declartion ka pollution ka hatne ka liya iife ka use karta hai 
+//syntax of iife is =()()
+//normal how to write a iife
+(function chai(){
+    //it is called a name IIFE
+    console.log(`DB CONNECTED`);
+})();//HERE YOU CAN END THE FUNCTION  other if you write a another function than shown a error
+//in arrow function function are also suppport
+//example
+((name) =>{
+    //simple IIFE
+    console.log(`DB CONNECTED TWO ${name}`);
+})('vikas')//this is the way to declare a variable
