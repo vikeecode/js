69bc7f8e52597b163ea2a50e1380624c4e84db0e
vikas <vikeepandit178@gmail.com>
2024-05-03 16:26:40 +0530

    this is control flow in switch case

---
+++ switch.js
@@ -0,0 +1,51 @@
+//switch condition
+//   switch (key) {
+//     case value:
+        
+//         break;
+  
+//     default:
+//         break;
+//   }
+// const month =3
+// switch (month) {
+//     case 1:
+//         console.log("jan");
+//         break;
+//     case 2:
+//         console.log("feb");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;   //if you not use break than all the code execute execpt default
+//     case 4:
+//         console.log("april");
+//         break;
+
+//     default://this case is like a that else
+//         console.log("default case match");
+//         break;
+// }
+
+//the value is string than
+const month ="march"   //if you can other data type than do same thing
+switch (month) {
+    case "jan":
+        console.log("jan");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        break;
+}
+
+//truthy and falsy value
+
